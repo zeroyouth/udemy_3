5c6c5f4ab50bfe0c1b3a73576684501e25809760
youngeun choi <choi46910@naver.com>
2022-07-14 22:53:49 +0900

    Edit : Login useEffect 수정

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('Cleanup!');
+      clearTimeout(identifier);
+    }; //클린업 프로세스
   }, [enteredEmail, enteredPassword]); //의존성 추가
 
   //유효성 검사
